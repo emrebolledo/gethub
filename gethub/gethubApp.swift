8626dd4abdb014f22088a802472ebc7148932687
Emma Rebolledo <emrebolledo17@gmail.com>
2023-07-13 13:33:57 -0500

    Initial Commit

---
+++ gethub/gethubApp.swift
@@ -0,0 +1,17 @@
+//
+//  gethubApp.swift
+//  gethub
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct gethubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
